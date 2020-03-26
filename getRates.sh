48dca3f01c0fbcc9fe0c9c994d1dbc412c3a101e
justin-brewer <jarvis@cpe-72-190-57-241.tx.res.rr.com>
2020-03-26 18:30:24 -0500

    change server to use fixer.io api

--- getRates.sh
+++ getRates.sh
@@ -1,3 +1,3 @@
 # bash script demonstrate the RESTful request executed by the server
-curl -X GET https://api.exchangeratesapi.io/latest
+curl -X GET http://data.fixer.io/api/latest?access_key=4196e8bc7ff14104a867e574540057a9&format=1
 echo ""
--- server/main.go
+++ server/main.go
@@ -21,12 +21,20 @@ var (
 	source = "get"
 )
 
+//const endpoint = "http://data.fixer.io/api/latest?access_key=4196e8bc7ff14104a867e574540057a9"
+const api_url = "http://data.fixer.io/api/latest"
+const api_key = "4196e8bc7ff14104a867e574540057a9"
+
+var endpoint = fmt.Sprintf("%v?access_key=%v", api_url, api_key)
+
 type server struct{}
 
 type xrates struct {
-	Rates map[string]float32
-	Base  string
-	Date  string
+	Success   bool
+	Timestamp int
+	Base      string
+	Date      string
+	Rates     map[string]float32
 }
 
 // SayHello implements helloworld.GreeterServer
@@ -38,7 +46,8 @@ func (s *server) GetConversion(ctx context.Context, in *pb.ConversionRequest) (*
 }
 
 func listCurrencies() string {
-
+	list := ""
+	return list
 }
 
 func getConversion(ctx context.Context, in *pb.ConversionRequest) float32 {
@@ -53,7 +62,7 @@ func getConversion(ctx context.Context, in *pb.ConversionRequest) float32 {
 func getRates() {
 	var rsp string
 	if source == "get" {
-		resp, err := http.Get("https://api.exchangeratesapi.io/latest")
+		resp, err := http.Get(endpoint)
 		if err != nil {
 			log.Printf("error with GET")
 			log.Printf("%v\n", err)
@@ -65,7 +74,7 @@ func getRates() {
 		}
 		rsp = string(body)
 	} else {
-		rsp = `{"rates":{"CAD":1.5524,"HKD":8.7466,"ISK":148.64,"PHP":57.645,"DKK":7.4727,"HUF":338.37,"CZK":26.203,"AUD":1.7674,"RON":4.8213,"SEK":10.8945,"IDR":16434.0,"INR":83.468,"BRL":5.5081,"RUB":84.0284,"HRK":7.6,"JPY":116.84,"THB":35.586,"CHF":1.0549,"SGD":1.5779,"PLN":4.3599,"BGN":1.9558,"TRY":7.0361,"CNY":7.8877,"NOK":11.3682,"NZD":1.8173,"ZAR":18.4447,"USD":1.124,"MXN":24.8028,"ILS":4.0909,"GBP":0.88623,"KRW":1359.4,"MYR":4.7944},"base":"EUR","date":"2020-03-12"}`
+		rsp = `{"success":true,"timestamp":1585261445,"base":"EUR","date":"2020-03-26","rates":{"AED":4.049721,"AFN":84.673933,"ALL":126.190057,"AMD":548.247044,"ANG":1.977177,"AOA":591.733047,"ARS":70.707016,"AUD":1.826082,"AWG":1.984644,"AZN":1.877768,"BAM":1.971106,"BBD":2.22708,"BDT":93.761897,"BGN":1.957068,"BHD":0.415387,"BIF":2090.491524,"BMD":1.10258,"BND":1.587575,"BOB":7.610279,"BRL":5.538588,"BSD":1.10457,"BTC":0.000164,"BTN":83.141858,"BWP":13.087382,"BYN":2.84671,"BYR":21610.566386,"BZD":2.220085,"CAD":1.550029,"CDF":1888.16528,"CHF":1.062335,"CLF":0.03313,"CLP":914.152134,"CNY":7.799541,"COP":4389.370652,"CRC":637.319176,"CUC":1.10258,"CUP":29.218368,"CVE":111.305632,"CZK":27.174624,"DJF":195.950458,"DKK":7.464086,"DOP":59.6714,"DZD":136.221534,"EGP":17.363651,"ERN":16.538525,"ETB":36.197879,"EUR":1,"FJD":2.532402,"FKP":0.908343,"GBP":0.907863,"GEL":3.782136,"GGP":0.908343,"GHS":6.334285,"GIP":0.908343,"GMD":56.13783,"GNF":10391.815887,"GTQ":8.746019,"GYD":231.124908,"HKD":8.547529,"HNL":27.614123,"HRK":7.612302,"HTG":104.299143,"HUF":355.724974,"IDR":17925.078343,"ILS":3.948758,"IMP":0.908343,"INR":82.379372,"IQD":1312.070102,"IRR":46424.127048,"ISK":154.008623,"JEP":0.908343,"JMD":149.536971,"JOD":0.781742,"JPY":120.639915,"KES":115.869724,"KGS":88.428559,"KHR":4470.961465,"KMF":495.471827,"KPW":992.39065,"KRW":1337.726607,"KWD":0.34114,"KYD":0.920459,"KZT":492.425087,"LAK":9848.243674,"LBP":1669.305763,"LKR":206.602374,"LRD":217.925518,"LSL":19.174144,"LTL":3.255632,"LVL":0.66694,"LYD":1.582243,"MAD":10.838783,"MDL":19.925267,"MGA":4130.264123,"MKD":62.176865,"MMK":1547.418432,"MNT":3059.395947,"MOP":8.820296,"MRO":393.621369,"MUR":43.169637,"MVR":16.990557,"MWK":810.395935,"MXN":25.730775,"MYR":4.808627,"MZN":73.409652,"NAD":19.173762,"NGN":404.646657,"NIO":37.536058,"NOK":11.487675,"NPR":133.028748,"NZD":1.855631,"OMR":0.424559,"PAB":1.104459,"PEN":3.74602,"PGK":3.770752,"PHP":56.876591,"PKR":181.236604,"PLN":4.520302,"PYG":7269.40543,"QAR":4.014475,"RON":4.834257,"RSD":117.502243,"RUB":85.422711,"RWF":1030.912223,"SAR":4.141394,"SBD":9.122788,"SCR":15.117289,"SDG":60.970245,"SEK":11.00981,"SGD":1.576023,"SHP":0.908343,"SLL":10708.804977,"SOS":645.008867,"SRD":8.223068,"STD":24314.943833,"SVC":9.663894,"SYP":567.224038,"SZL":19.174269,"THB":35.946929,"TJS":11.271856,"TMT":3.870056,"TND":3.175675,"TOP":2.614987,"TRY":7.053816,"TTD":7.463242,"TWD":33.282482,"TZS":2551.481109,"UAH":31.170132,"UGX":4291.077752,"USD":1.10258,"UYU":49.46636,"UZS":10491.048092,"VEF":11.01202,"VND":25630.021476,"VUV":138.518054,"WST":3.074351,"XAF":661.06824,"XAG":0.076723,"XAU":0.000676,"XCD":2.979777,"XDR":0.816614,"XOF":663.753047,"XPF":120.649772,"YER":276.031219,"ZAR":19.102743,"ZMK":9924.541857,"ZMW":19.683266,"ZWL":355.030735}}`
 	}
 
 	errR := json.Unmarshal([]byte(rsp), &rates)

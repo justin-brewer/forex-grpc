03a67aef0bdae997700fa33c8203b6d914132d1e
justin-brewer <jarvis@cpe-72-190-57-241.tx.res.rr.com>
2020-03-26 21:53:40 -0500

    update get-dependencies.ps1

--- get-dependencies.ps1
+++ get-dependencies.ps1
@@ -1,12 +1,13 @@
 # powershell script to install go dependencies
-# *untested
+# untested
 go get -u   google.golang.org/grpc `
             google.golang.org/grpc/codes `
             google.golang.org/grpc/status `
+	    gopkg.in/yaml.v2 `
             google.golang.org/grpc/metadata `
             google.golang.org/grpc/reflection `
             google.golang.org/grpc/examples/helloworld/helloworld `
             golang.org/x/net/context `
             golang.org/x/oauth2/google `
-            github.com/golang/protobuf/proto
-
+            github.com/golang/protobuf/proto `
+	    github.com/golang/protobuf/protoc-gen-go
